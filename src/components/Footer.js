c4cddc163b69f027023e4234c86ba27f0cd5010b
kavinduaj12 <86953304+kavinduaj12@users.noreply.github.com>
2021-07-20 16:06:39 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,7 @@ class Footer extends Component {
         <div className="foot">
           
  
-          {/* All the links in the footer should remain intact.
-          You can delete the links only if you Have permission.
-          Licensing information: https://github.com/vimalverma558/Master-Portfolio/blob/master/LICENSE  
-          https://github.com/vimalverma558/Master-Portfolio */}
+   
 
 
           <p>Made with <a href="https://www.kavinduaj1.tk">Master-Portfolio</a> <span role="img" aria-label="Heart"> ❤️ </span> by { greeting.full_name } </p>
